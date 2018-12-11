39d450253ef1384d49cc7c2ba61faa314e753169
Don Denton <don@happycollision.com>
2018-12-10 18:09:19 -0600

    Playlist of videos...
    - VideoPlayer plays the videos back to back (depends on props not changing, I think)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import Counter from './components/Counter';
 import VideoPlayer from './components/VideoPlayer';
 
-export default () => (
-  <div className="h-screen w-screen bg-blue-darkest">
-    <Counter className="bg-green-darker" />
-    <VideoPlayer videos={['FrixxerVids/BrainBlitz.mp4']} />
-  </div>
-);
+export default () => {
+  const videos = ['BrainBlitz', 'LyingGame', 'NeedToKnow'].map(
+    x => `FrixxerVids/${x}.mp4`,
+  );
+  return (
+    <div className="h-screen w-screen bg-blue-darkest">
+      <Counter className="bg-green-darker" />
+      <VideoPlayer videos={videos} />
+    </div>
+  );
+};
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   videos: string[];
 }
 
 export default ({ videos }: Props) => {
-  return <video src={videos[0]} autoPlay />;
+  const [videoList, setVideoList] = useState(videos);
+
+  const nextVid = () => {
+    const [head, ...tail] = videoList;
+    setVideoList(tail);
+  };
+
+  return (
+    <video
+      className="max-w-full"
+      src={videoList[0]}
+      autoPlay
+      onEnded={nextVid}
+    />
+  );
 };
