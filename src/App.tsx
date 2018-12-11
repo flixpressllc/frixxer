a14f6df4ef77083e17a3178a45b6cc720f1a6f2f
Don Denton <don@happycollision.com>
2018-12-11 11:19:11 -0600

    Append videos to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './components/Counter';
 import VideoPlayer from './components/VideoPlayer';
 
+const addLocation = (...vids: string[]): string[] =>
+  vids.map(x => `FrixxerVids/${x}.mp4`);
+
 export default () => {
-  const videos = ['BrainBlitz', 'LyingGame', 'NeedToKnow'].map(
-    x => `FrixxerVids/${x}.mp4`,
-  );
+  const videos = addLocation('BrainBlitz', 'LyingGame', 'NeedToKnow');
+  const videos2 = addLocation('PintSizeReplays', 'RulesRegs');
+  const [currentVids, setCurrentVids] = useState(videos);
+
+  setTimeout(() => setCurrentVids(videos2), 3000);
+
   return (
     <div className="h-screen w-screen bg-blue-darkest">
       <Counter className="bg-green-darker" />
-      <VideoPlayer videos={videos} />
+      <VideoPlayer videos={currentVids} />
     </div>
   );
 };
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { deepEqual } from 'happy-helpers';
 
 interface Props {
   videos: string[];
@@ -6,18 +7,30 @@ interface Props {
 
 export default ({ videos }: Props) => {
   const [videoList, setVideoList] = useState(videos);
+  const [lastProps, setLastProps] = useState(videos);
 
   const nextVid = () => {
     const [head, ...tail] = videoList;
     setVideoList(tail);
   };
 
+  useEffect(() => {
+    if (deepEqual(lastProps, videos)) {
+      return;
+    }
+    setLastProps(videos);
+    setVideoList([...videoList, ...videos]);
+  });
+
   return (
-    <video
-      className="max-w-full"
-      src={videoList[0]}
-      autoPlay
-      onEnded={nextVid}
-    />
+    <div className="text-white">
+      {videoList}
+      <video
+        className="max-w-full"
+        src={videoList[0]}
+        autoPlay
+        onEnded={nextVid}
+      />
+    </div>
   );
 };
