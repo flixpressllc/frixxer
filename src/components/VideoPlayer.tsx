fb33b962489b2715ee5adba355c5855b617f2575
Don Denton <don@happycollision.com>
2018-12-12 13:00:31 -0600

    Players are keyed and referenced...
    This will allow me to swap the frontmost player when conditions are right

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, SyntheticEvent } from 'react';
+import React, { useState, SyntheticEvent, useRef, useEffect } from 'react';
 import { deepEqual } from 'happy-helpers';
 
 interface Props {
@@ -9,7 +9,7 @@ export default ({ videos }: Props) => {
   const [videoList, setVideoList] = useState(videos);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const nextVid = () => {
+  const handleEnded = () => {
     if (currentIndex < videoList.length - 1) {
       setCurrentIndex(currentIndex + 1);
     } else {
@@ -17,24 +17,88 @@ export default ({ videos }: Props) => {
     }
   };
 
-  const currentVideo = currentIndex > -1 ? videoList[currentIndex] : undefined;
+  const currentSrc = currentIndex > -1 ? videoList[currentIndex] : undefined;
   const [lastProps, setLastProps] = useState(videos);
   if (!deepEqual(lastProps, videos)) {
     setLastProps(videos);
-    if (!currentVideo) {
+    if (!currentSrc) {
       setCurrentIndex(videoList.length);
     }
     setVideoList([...videoList, ...videos]);
   }
 
+  const nextSrc =
+    currentSrc && videoList[currentIndex + 1]
+      ? videoList[currentIndex + 1]
+      : undefined;
+
   const [blocked, setBlocked] = useState(false);
-  const play = (ev: SyntheticEvent<HTMLVideoElement>) => {
-    ev.currentTarget.play().catch(() => {
+  const play = (playerEl: HTMLVideoElement) => {
+    if (!playerEl.paused) {
+      return;
+    }
+    playerEl.play().catch(() => {
       // Autoplay is blocked, likely due to lack of interaction
       setBlocked(true);
     });
   };
-  const notBlocked = () => setBlocked(false);
+  const notBlocked = () => (blocked ? setBlocked(false) : void 0);
+
+  const player1: React.MutableRefObject<null | HTMLVideoElement> = useRef(null);
+  const player2: React.MutableRefObject<null | HTMLVideoElement> = useRef(null);
+  const [player1Ready, setPlayer1Ready] = useState(false);
+  const [player2Ready, setPlayer2Ready] = useState(false);
+  function reportReady(playerEl: HTMLVideoElement) {
+    if (playerEl === player1.current) {
+      setPlayer1Ready(true);
+    }
+    if (playerEl === player2.current) {
+      setPlayer2Ready(true);
+    }
+  }
+  function reportPlaying(playerEl: HTMLVideoElement) {
+    notBlocked();
+    if (playerEl === player1.current) {
+      setPlayer1Ready(false);
+    }
+    if (playerEl === player2.current) {
+      setPlayer2Ready(false);
+    }
+  }
+  useEffect(
+    () => {
+      [player1, player2].forEach(ref => {
+        if (!ref.current) {
+          throw new Error('players were not properly referenced');
+        }
+        const endMatch = new RegExp(currentSrc + '$');
+        if (endMatch.test(ref.current.src)) {
+          const playerReady = ref === player1 ? player1Ready : player2Ready;
+          if (playerReady) {
+            play(ref.current);
+          }
+        }
+      });
+    },
+    [currentSrc, nextSrc, player1Ready, player2Ready, player1, player2],
+  );
+  function player(key: number, ref: any, initialSrc: string | undefined) {
+    // const isCurrent = key === currentPlayerKey.current;
+    // const className = isCurrent ? 'max-w-full relative z-10' : 'absolute max-w-full z-0';
+    const className = 'max-w-1/2 relative z-10';
+    return (
+      <video
+        ref={ref}
+        key={key}
+        className={className}
+        src={initialSrc}
+        onEnded={handleEnded}
+        controls={blocked}
+        onCanPlayThrough={ev => reportReady(ev.currentTarget)}
+        onPlay={ev => reportPlaying(ev.currentTarget)}
+      />
+    );
+  }
 
   return (
     <div className="text-white">
@@ -47,17 +111,15 @@ export default ({ videos }: Props) => {
           ))}
         </ol>
         <div>
-          Index: {currentIndex + 1} - {currentVideo}
+          Index: {currentIndex + 1} - {currentSrc}
         </div>
       </div>
-      <video
-        className="max-w-full"
-        src={currentVideo}
-        onEnded={nextVid}
-        controls={blocked}
-        onCanPlayThrough={play}
-        onPlay={notBlocked}
-      />
+      <div className="relative max-w-full">
+        {[[1, player1, currentSrc], [2, player2, nextSrc]].map(x =>
+          // @ts-ignore
+          player(...x),
+        )}
+      </div>
     </div>
   );
 };
