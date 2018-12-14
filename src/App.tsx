018f7c4e4721b781a940c61546ddfddbdfb1e855
Don Denton <don@happycollision.com>
2018-12-14 17:50:11 -0600

    Remove counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import Counter from './components/Counter';
+import React from 'react';
 import VideoPlayer from './components/VideoPlayer';
 import { useVideoFeed } from './utils/dev-utils';
 import Ticker from './components/Ticker';
@@ -9,7 +8,6 @@ export default () => {
 
   return (
     <div className="h-screen w-screen bg-blue-darkest text-white">
-      <Counter className="bg-green-darker" />
       <VideoPlayer videos={videos} />
       <Ticker items={['Test One', 'Test Two']} />
     </div>
