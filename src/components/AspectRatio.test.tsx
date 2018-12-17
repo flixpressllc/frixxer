c93730936bdc73082dc6aa09c740fb74d0710114
Don Denton <don@happycollision.com>
2018-12-17 16:19:27 -0600

    Add AspectRatio component

---
+++ src/components/AspectRatio.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AspectRatio from './AspectRatio';
+import { render } from 'react-testing-library';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AspectRatio ratio="4:3">Hi</AspectRatio>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('creates a div with the appropriate padding', () => {
+  const { getByText, rerender } = render(
+    <AspectRatio ratio="16:9">Things</AspectRatio>,
+  );
+
+  const ratioDiv = getByText('Things').parentElement;
+  expect(ratioDiv).toHaveStyle('padding-bottom: 56.25%');
+
+  rerender(<AspectRatio ratio="4:3">Things</AspectRatio>);
+  expect(ratioDiv).toHaveStyle('padding-bottom: 75%');
+});
+
+it('allows direct className association', () => {
+  const { getByText } = render(
+    <AspectRatio ratio="16:9" className="bg-grey">
+      Things
+    </AspectRatio>,
+  );
+
+  const ratioDiv = getByText('Things').parentElement;
+  expect(ratioDiv).toHaveClass('bg-grey');
+});
---
+++ src/components/AspectRatio.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { removeProps } from '../utils';
+
+interface ComponentProps {
+  ratio: string;
+}
+
+interface Props extends ComponentProps, React.HTMLAttributes<HTMLDivElement> {}
+
+/**
+ *
+ * @param props includes a `ratio` property which is a string in the form of width:height. Eg. "16:9", "4:3"
+ *
+ * @example
+ *
+ * <AspectRatio ratio='16:9'>
+ *   // Your children here
+ * </AspectRatio>
+ */
+export default function AspectRatio(props: Props) {
+  const ratioAsNumber = props.ratio
+    .split(':')
+    .map(str => +str)
+    .reduce((prev, curr) => curr / prev);
+
+  const divProps = removeProps(props, 'children', 'ratio');
+  return (
+    <div
+      {...divProps}
+      style={{
+        ...(divProps.style || {}),
+        paddingBottom: `${ratioAsNumber * 100}%`,
+      }}
+      className={`relative ${divProps.className || ''}`}
+    >
+      <div className="absolute w-full h-full pin">{props.children}</div>
+    </div>
+  );
+}
