8dbb5e76ec8fd627507307bd3f3c1af54fb6ae37
Don Denton <don@happycollision.com>
2018-12-19 18:27:48 -0600

    Simplify the type of removeProps

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,7 @@
-type ObjectExclude<T, U> = Pick<T, Exclude<keyof T, keyof U>>;
-
-export function removeProps<
-  T extends object,
-  U extends keyof T,
-  Z = { [P in U]: any }
->(obj: T, ...propNames: U[]): ObjectExclude<T, Z> {
+export function removeProps<T extends object, U extends keyof T>(
+  obj: T,
+  ...propNames: U[]
+): Pick<T, Exclude<keyof T, U>> {
   const objClone = { ...obj };
   propNames.forEach(p => delete objClone[p]);
   return objClone;
