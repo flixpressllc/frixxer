7f20025dee384220fdb2b943e5984bf971540f0d
Don Denton <don@happycollision.com>
2018-12-21 15:31:08 -0600

    Remove news layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import DefaultLayout from './components/DefaultLayout';
-import NewsLayout from './components/NewsLayout';
 
 export default function App() {
-  return <div>{false ? <DefaultLayout /> : <NewsLayout />}</div>;
+  return (
+    <div>
+      <DefaultLayout />
+    </div>
+  );
 }
