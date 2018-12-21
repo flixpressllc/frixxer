22f6ef59c21d01b0758e2787206c1858a9be2203
Don Denton <don@happycollision.com>
2018-12-21 16:20:43 -0600

    Add a mergeCLasses function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,18 @@ export function removeProps<T extends object, U extends keyof T>(
   propNames.forEach(p => delete objClone[p]);
   return objClone;
 }
+
+export function mergeClasses(
+  ...classes: Array<string | { className?: string }>
+): string {
+  return classes.reduce((result: string, className) => {
+    let newClasses: string | undefined;
+    if (typeof className === 'string') {
+      newClasses = className;
+    } else {
+      newClasses = className.className;
+    }
+
+    return newClasses ? `${result} ${newClasses}` : result;
+  }, '');
+}
