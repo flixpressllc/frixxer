d27bcf1f2f19ae9f651a56a22769fa7756059dd9
Don Denton <don@happycollision.com>
2018-12-31 13:58:31 -0600

    Better timing for countdown

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -14,17 +14,19 @@ export default function DefaultLayout(props: Props) {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
   const [countdown, setCountdown] = useState(30);
+  const r = useMutable({ countdown, setCountdown });
   useEffect(
     () => {
-      const timeoutNumber = setTimeout(() => {
-        const newTime = countdown - 1 === -1 ? 30 : countdown - 1;
-        setCountdown(newTime);
+      const timeoutNumber = setInterval(() => {
+        const newTime =
+          r.current.countdown - 1 === 0 ? 30 : r.current.countdown - 1;
+        r.current.setCountdown(newTime);
       }, 1000);
       return () => {
-        clearTimeout(timeoutNumber);
+        clearInterval(timeoutNumber);
       };
     },
-    [countdown],
+    ['once'],
   );
 
   return (
