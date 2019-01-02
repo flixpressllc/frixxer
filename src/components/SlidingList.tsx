258520bce1e63e960ddc184446c51f857ceb8d70
Don Denton <don@happycollision.com>
2019-01-02 13:10:30 -0600

    swap shading on items in sliding list

--- src/components/SlidingList.tsx
+++ src/components/SlidingList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 interface Item {
   label: string;
@@ -8,6 +8,13 @@ interface Item {
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   items: Item[];
 }
+
+const getItemShadingA = (i: number): string => {
+  return i % 2 ? 'bg-shade-light' : '';
+};
+const getItemShadingB = (i: number): string => {
+  return i % 2 ? '' : 'bg-shade-light';
+};
 
 export default function SlidingList(props: Props) {
   const [list, setList] = useState(props.items);
@@ -28,12 +35,28 @@ export default function SlidingList(props: Props) {
     },
     [props.items],
   );
+
+  const getItemShading = useRef(getItemShadingA);
+  useEffect(
+    () => {
+      if (previousList[0] && list[0] && previousList[0].id === list[0].id) {
+        return;
+      }
+      if (previousList[1] && list[0] && previousList[1].id === list[0].id) {
+        getItemShading.current =
+          getItemShading.current === getItemShadingA
+            ? getItemShadingB
+            : getItemShadingA;
+      }
+    },
+    [props.items],
+  );
   return (
     <div {...props}>
       {masterList.map((item, i) => (
         <li
           key={item.id}
-          className={`flex text-2xl p-4 ${i % 2 ? 'bg-shade-light' : ''}`}
+          className={`flex text-2xl p-4 ${getItemShading.current(i)}`}
         >
           <div className="px-4 flex-grow">{item.label}</div>
           <div className="px-4">12s</div>
