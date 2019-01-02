06a20369908c04cca3e213a34bdcfd4b6609cc0f
Don Denton <don@happycollision.com>
2019-01-02 11:03:51 -0600

    sliding list appends new lists with previous first item

--- src/components/SlidingList.tsx
+++ src/components/SlidingList.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+type Item = string;
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
-  items: any[];
+  items: Item[];
 }
 
 export default function SlidingList(props: Props) {
+  const [list, setList] = useState(props.items);
+  const [previousList, setPreviousList] = useState([] as Item[]);
+  function combineLists(prev: Item[], next: Item[]): Item[] {
+    return prev[0] ? [prev[0]].concat(next) : next;
+  }
+  const [masterList, setMasterList] = useState(
+    combineLists(previousList, list),
+  );
+  useEffect(
+    () => {
+      if (list !== props.items) {
+        setPreviousList(list);
+        setList(props.items);
+        setMasterList(combineLists(list, props.items));
+      }
+    },
+    [props.items],
+  );
   return (
     <div {...props}>
-      {props.items.map((name, i) => (
+      {masterList.map((name, i) => (
         <li
           key={i}
           className={`flex text-2xl p-4 ${i % 2 ? 'bg-shade-light' : ''}`}
