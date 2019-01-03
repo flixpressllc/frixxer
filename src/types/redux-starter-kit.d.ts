6de64ed7e9b0f4fea1402cbc7e01e275972db9ac
Don Denton <don@happycollision.com>
2019-01-03 16:28:11 -0600

    types: add createReducer

--- src/types/redux-starter-kit.d.ts
+++ src/types/redux-starter-kit.d.ts
@@ -2,6 +2,14 @@ type AnyFn = (...args: any[]) => any;
 
 declare module 'redux-starter-kit' {
   import Redux from 'redux';
+
+  function createReducer<T extends any>(
+    initialState: T,
+    actionsMap: {
+      [s: string]: (state: T, action: Redux.AnyAction) => T | void;
+    },
+  ): Redux.Reducer;
+
   const configureStore: (
     input: {
       reducer: { [s: string]: Redux.Reducer } | Redux.Reducer;
