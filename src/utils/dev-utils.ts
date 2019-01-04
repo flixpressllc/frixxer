7ab59cb8440d3fcd47e4cd68af8ca4d2d5deff0c
Don Denton <don@happycollision.com>
2019-01-03 18:15:34 -0600

    update devUtils...
    no longer need some stuff functions
    
    updating to take advantage of labeling

--- src/utils/dev-utils.ts
+++ src/utils/dev-utils.ts
@@ -1,70 +1,25 @@
-import { useEffect, useState } from 'react';
-import { shuffleClone, clone } from 'happy-helpers';
-
-function addLocation(...vids: string[]): string[] {
-  return vids.map(x => `FrixxerVids/${x}.mp4`);
+function addLocation(vid: string): string {
+  return `FrixxerVids/${vid}.mp4`;
 }
 
-export const devVideos = [
-  'BrainBlitz',
-  'LyingGame',
-  'NeedtoKnow',
-  'PintSizeReplays',
-  'RulesRegs',
-  'SumCity',
-  'SundayShocker',
-  'TheFeed',
-  'TheScramble',
-  'ThrillsSpills',
-  'TrickMyTailgate',
-  'WhoSaidIt',
-  'XsOs',
+const devVideos = [
+  { label: 'Brain Blitz', url: 'BrainBlitz' },
+  { label: 'The Lying Game', url: 'LyingGame' },
+  { label: 'Need to Know', url: 'NeedtoKnow' },
+  { label: 'Pint Size Replays', url: 'PintSizeReplays' },
+  { label: 'Rules & Regs', url: 'RulesRegs' },
+  { label: 'Sum City', url: 'SumCity' },
+  { label: 'Sunday Shocker', url: 'SundayShocker' },
+  { label: 'The Feed', url: 'TheFeed' },
+  { label: 'The Scramble', url: 'TheScramble' },
+  { label: 'Thrills & Spills', url: 'ThrillsSpills' },
+  { label: 'Trick My Tailgate', url: 'TrickMyTailgate' },
+  { label: 'Who Said It', url: 'WhoSaidIt' },
+  { label: "X's and O's", url: 'XsOs' },
 ];
 
-export const labeledDevVideos = devVideos.map((name, i) => ({
-  label: name,
-  url: addLocation(name),
+export const labeledDevVideos = devVideos.map(({ label, url }, i) => ({
+  label,
+  url: addLocation(url),
   id: i,
 }));
-
-export function useVideoFeed() {
-  const allVideos = addLocation(...shuffleClone(devVideos));
-  const [currentVids, setCurrentVids] = useState([] as string[]);
-
-  useEffect(
-    () => {
-      const [vid1, ...remaining] = allVideos;
-      setTimeout(() => setCurrentVids([vid1]), 500);
-      setTimeout(() => setCurrentVids(remaining), 3500);
-    },
-    ['once'],
-  );
-
-  return [currentVids, setCurrentVids] as [
-    string[],
-    React.Dispatch<React.SetStateAction<string[]>>
-  ];
-}
-
-export function useTimedList(
-  intervalTime: number = 1000,
-  list: string[] = devVideos,
-) {
-  const [currentList, setCurrentList] = useState(labeledDevVideos);
-
-  useEffect(
-    () => {
-      const id = setTimeout(() => {
-        const newItem = clone(currentList[0]);
-        newItem.id = currentList[currentList.length - 1].id + 1;
-        setCurrentList(currentList.concat(newItem).splice(1));
-      }, intervalTime);
-      return () => {
-        clearTimeout(id);
-      };
-    },
-    [intervalTime, currentList],
-  );
-
-  return currentList;
-}
