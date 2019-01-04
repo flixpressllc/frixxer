349dfcb976cfae63243da6ae722eab96a0de347d
Don Denton <don@happycollision.com>
2019-01-04 14:13:57 -0600

    add api file...
    this is where we can kick all the dev stuff to

---
+++ src/api.ts
@@ -0,0 +1,16 @@
+import { fetchVideoData } from './utils/dev-utils';
+
+const lipsum =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+export async function loadData() {
+  const videos = await fetchVideoData();
+  return {
+    videos,
+    tickers: {
+      1: ['Global Headlines:', lipsum],
+      2: ['Entertainment Headlines', lipsum],
+      3: ['Sports Headlines', lipsum],
+    },
+  };
+}
