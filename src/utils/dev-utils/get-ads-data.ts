800f6fe23760c4670a342a8c3f35f6b0ffba01a2
Don Denton <don@happycollision.com>
2019-01-07 17:54:07 -0600

    return ads data in loadData()

---
+++ src/utils/dev-utils/get-ads-data.ts
@@ -0,0 +1,12 @@
+export function getAdsData() {
+  return {
+    core1: ['http://lorempixel.com/abstract/200/50/core-1'],
+    core2: ['http://lorempixel.com/abstract/200/50/core-2'],
+    banner: ['http://lorempixel.com/food/880/50'],
+    main: [
+      'https://fillmurray.com/480/317',
+      'https://www.placecage.com/480/317',
+      'https://www.stevensegallery.com/480/317',
+    ],
+  };
+}
