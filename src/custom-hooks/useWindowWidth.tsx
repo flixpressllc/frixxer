0d187e8cade99eb1b199ab893057104608f1e0cc
Don Denton <don@happycollision.com>
2019-01-07 12:11:36 -0600

    make useWindowWidth more performant

--- src/custom-hooks/useWindowWidth.tsx
+++ src/custom-hooks/useWindowWidth.tsx
@@ -1,13 +1,38 @@
 import { useState, useEffect } from 'react';
+import { useMutable } from './useMutable';
+
+const listeners = new Set<{ current: (width: number) => void }>();
+
+function perform() {
+  listeners.forEach(ref => ref.current(window.innerWidth));
+}
+
+function addWindowSizeListener(setStateRef: {
+  current: (width: number) => void;
+}) {
+  listeners.add(setStateRef);
+  if (listeners.size === 1) {
+    window.addEventListener('resize', perform);
+  }
+}
+
+function removeWindowSizeListener(setStateRef: {
+  current: (width: number) => void;
+}) {
+  listeners.delete(setStateRef);
+  if (listeners.size === 0) {
+    window.removeEventListener('resize', perform);
+  }
+}
 
 export function useWindowWidth() {
   const [width, setWidth] = useState(window.innerWidth);
+  const setWidthRef = useMutable(setWidth);
   useEffect(
     () => {
-      const handleResize = () => setWidth(window.innerWidth);
-      window.addEventListener('resize', handleResize);
+      addWindowSizeListener(setWidthRef);
       return () => {
-        window.removeEventListener('resize', handleResize);
+        removeWindowSizeListener(setWidthRef);
       };
     },
     ['once'],
