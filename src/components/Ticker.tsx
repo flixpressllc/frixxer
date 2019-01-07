aba817e21d9e51b113270015f4ad49fba31fabaf
Don Denton <don@happycollision.com>
2019-01-07 12:29:52 -0600

    remove unused import

--- src/components/Ticker.tsx
+++ src/components/Ticker.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Ticker.css';
 import { nullDispatch, removeNonAttributePropsAnd } from '../utils';
 import { StoreData } from '../redux/store';
