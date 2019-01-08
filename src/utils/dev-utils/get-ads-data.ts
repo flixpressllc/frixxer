1ba4d77c9dd77d26d0ecc299a2e6994dde9ebc6a
Don Denton <don@happycollision.com>
2019-01-08 13:25:15 -0600

    fix lorem px data

--- src/utils/dev-utils/get-ads-data.ts
+++ src/utils/dev-utils/get-ads-data.ts
@@ -1,8 +1,8 @@
 export function getAdsData() {
   return {
-    core1: ['http://lorempixel.com/abstract/200/50/core-1'],
-    core2: ['http://lorempixel.com/abstract/200/50/core-2'],
-    banner: ['http://lorempixel.com/food/880/50'],
+    core1: ['http://lorempixel.com/200/50/abstract/core-1'],
+    core2: ['http://lorempixel.com/200/50/abstract/core-2'],
+    banner: ['http://lorempixel.com/880/50/food'],
     main: [
       'https://fillmurray.com/480/317',
       'https://www.placecage.com/480/317',
